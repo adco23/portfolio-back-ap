7c8ee8fcbb5c8d0b5afa2f9216c4f0fbc742e354
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-03 23:12:31 -0300

    ✨welcome eendpoint

---
+++ src/main/java/com/portfolio/AC/controllers/StartController.java
@@ -0,0 +1,22 @@
+package com.portfolio.AC.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/")
+public class StartController {
+  @GetMapping("")
+  public ResponseEntity<?> welcome(){
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body("Welcome to portfolio");
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+  }
+}
