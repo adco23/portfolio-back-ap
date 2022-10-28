eef47346d6fccc075b34aad5d09cf5dd59f80c5b
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-28 10:54:05 -0300

    project cross origin add

--- src/main/java/com/portfolio/AC/controllers/ProjectController.java
+++ src/main/java/com/portfolio/AC/controllers/ProjectController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/projects")
 public class ProjectController {
