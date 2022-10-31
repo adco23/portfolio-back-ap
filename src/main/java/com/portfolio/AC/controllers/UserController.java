08710806536cb670dee8d6485c2aa91ee92c102e
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-31 18:26:03 -0300

    VARIABLES

--- src/main/java/com/portfolio/AC/controllers/UserController.java
+++ src/main/java/com/portfolio/AC/controllers/UserController.java
@@ -9,10 +9,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "${FRONT_URL}")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
